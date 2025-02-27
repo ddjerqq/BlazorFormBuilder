ec66eed99601901fa286353391dee0bb76683add
ddjerqq <57017344+ddjerqq@users.noreply.github.com>
2025-02-28 03:44:37 +0400

    transformed the prototype into a Blazor WASM solution. added the ability to edit the selected components.

---
+++ src/Client/Pages/Create/BaseComponentChoice.cs
@@ -0,0 +1,111 @@
+﻿using System.Security.Cryptography;
+
+namespace Client.Pages.Create;
+
+public abstract class BaseComponentChoice
+{
+    public string Id { get; set; } = RandomNumberGenerator.GetHexString(5);
+    public required string Label { get; set; }
+    public bool Required { get; set; } = false;
+
+    public static BaseComponentChoice CreateDefault(Type componentType)
+    {
+        return componentType.Name switch
+        {
+            "TextInput" => new TextInput
+            {
+                InputType = "text",
+                Placeholder = "...",
+                Label = "TextInput",
+            },
+            "NumberInput" => new NumberInput
+            {
+                InputType = "number",
+                Label = "NumberInput",
+            },
+            "SelectInput" => new SelectInput
+            {
+                Choices = ["a", "b", "c"],
+                Label = "SelectInput",
+            },
+            "CheckboxInput" => new CheckboxInput
+            {
+                Label = "CheckboxInput",
+            },
+            "DateInput" => new DateInput
+            {
+                InputType = "datetime-local",
+                Label = "DateInput",
+            },
+            "Button" => new Button
+            {
+                ButtonText = "Hire me!",
+                Label = "Button",
+            },
+            _ => throw new ArgumentException("Invalid component type"),
+        };
+    }
+}
+
+public sealed class TextInput : BaseComponentChoice
+{
+    private string _inputType = null!;
+
+    public required string InputType
+    {
+        get => _inputType;
+        set
+        {
+            if (value is not ("text" or "tel" or "url" or "password" or "search"))
+                throw new ArgumentException("InputType must be one of the following: text, tel, url, password, search");
+
+            _inputType = value;
+        }
+    }
+
+    public required string Placeholder { get; set; }
+}
+
+public sealed class NumberInput : BaseComponentChoice
+{
+    public float Min { get; set; } = 0;
+    public float Max { get; set; } = 1;
+    public float Step { get; set; } = 0.1f;
+    public required string InputType { get; set; }
+}
+
+public sealed class SelectInput : BaseComponentChoice
+{
+    public required List<string> Choices { get; set; }
+}
+
+public sealed class CheckboxInput : BaseComponentChoice;
+
+/// <summary>
+/// Only allowed values for InputType are: date, month, week, time, datetime-local
+/// </summary>
+public sealed class DateInput : BaseComponentChoice
+{
+    private string _inputType = null!;
+
+    public required string InputType
+    {
+        get => _inputType;
+        set
+        {
+            if (value is not ("date" or "month" or "week" or "time" or "datetime-local"))
+                throw new ArgumentException("InputType must be one of the following: date, month, week, time, datetime-local");
+
+            _inputType = value;
+        }
+    }
+}
+
+/// <summary>
+/// The type="submit" button for the form.
+/// We could also have buttons of type="button" but that is outside the scope of this project
+/// </summary>
+public sealed class Button : BaseComponentChoice
+{
+    public required string ButtonText { get; set; }
+}
---
+++ src/Client/Pages/Create/Create.razor.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace Client.Pages.Create;
+
+public partial class Create
+{
+    private BaseComponentChoice? CurrentlyEditing { get; set; }
+
+    private static readonly List<Type> PickerComponents =
+    [
+        typeof(TextInput),
+        typeof(NumberInput),
+        typeof(SelectInput),
+        typeof(CheckboxInput),
+        typeof(DateInput),
+        typeof(Button),
+    ];
+
+    private readonly List<BaseComponentChoice> _builderItems = [];
+
+    private bool _isDraggingFromPicker;
+    private int? _draggingIndex;
+    private Type? _draggingComponentType;
+
+    private void OnPickerDragStart(Type compType)
+    {
+        _draggingComponentType = compType;
+        _isDraggingFromPicker = true;
+    }
+
+    private void OnBuilderDragStart(int index)
+    {
+        _draggingIndex = index;
+        _isDraggingFromPicker = false;
+    }
+
+    private void OnDragEnd()
+    {
+        _draggingIndex = null;
+        _draggingComponentType = null;
+        _isDraggingFromPicker = false;
+    }
+
+    /// <summary>
+    /// even though this method does absolutely nothing, it is required for blazor to fire the appropriate event
+    /// </summary>
+    private void OnDropZoneDragOver(DragEventArgs e)
+    {
+    }
+
+    private void OnDropAt(int index)
+    {
+        if (_isDraggingFromPicker && _draggingComponentType != null)
+        {
+            var component = BaseComponentChoice.CreateDefault(_draggingComponentType);
+            _builderItems.Insert(index, component);
+            CurrentlyEditing = component;
+        }
+        else if (!_isDraggingFromPicker && _draggingIndex.HasValue)
+        {
+            var item = _builderItems[_draggingIndex.Value];
+            _builderItems.RemoveAt(_draggingIndex.Value);
+
+            if (_draggingIndex.Value < index)
+            {
+                index--;
+            }
+
+            _builderItems.Insert(index, item);
+        }
+
+        OnDragEnd();
+        StateHasChanged();
+    }
+
+    private void RemoveItem(int index)
+    {
+        _builderItems.RemoveAt(index);
+        StateHasChanged();
+    }
+
+    private void EditItem(int index)
+    {
+        CurrentlyEditing = _builderItems[index];
+        StateHasChanged();
+    }
+}
